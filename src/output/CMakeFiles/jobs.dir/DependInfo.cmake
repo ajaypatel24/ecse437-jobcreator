ae506bcef1a201b7464781492925984adac821ef
ajaypatel24 <ajay.patel@mail.mcgill.ca>
2020-09-29 12:12:08 -0400

    finished

---
+++ src/output/CMakeFiles/jobs.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/ajaypatel/Desktop/ECSE437/Labs/Lab2/ecse437-jobcreator/src/jobs/job.c" "/Users/ajaypatel/Desktop/ECSE437/Labs/Lab2/ecse437-jobcreator/src/output/CMakeFiles/jobs.dir/jobs/job.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/output/CMakeFiles/jobs.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for jobs.
+# This may be replaced when dependencies are built.
